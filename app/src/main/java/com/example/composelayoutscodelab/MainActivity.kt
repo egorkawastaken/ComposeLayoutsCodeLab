2a0b444514c29afeb6b2d1f2f0773359f79e013d
Egor <egordusik@gmail.com>
2021-12-29 14:37:49 +0300

    Simple Custom Layout

--- app/src/main/java/com/example/composelayoutscodelab/MainActivity.kt
+++ app/src/main/java/com/example/composelayoutscodelab/MainActivity.kt
@@ -3,90 +3,53 @@ package com.example.composelayoutscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
 import com.example.composelayoutscodelab.ui.theme.ComposeLayoutsCodeLabTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import kotlinx.coroutines.launch
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLayoutsCodeLabTheme {
-                SimpleList()
-            }
-        }
-    }
-}
-
-
-@Composable
-fun SimpleList() {
-    val coroutineScope = rememberCoroutineScope()
-    val scrollState = rememberLazyListState()
-    val listSize = 100
-
-    Column() {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Button(
-                onClick = {
-                    coroutineScope.launch {
-                        scrollState.animateScrollToItem(0)
-                    }
-                }
-            ) {
-                Text("Scroll to the top")
-            }
-
-            Button(
-                onClick = {
-                    coroutineScope.launch {
-                        scrollState.animateScrollToItem(listSize - 1)
-                    }
-                }
-            ) {
-                Text("Scroll to the bottom")
-            }
-        }
-
-        LazyColumn(state = scrollState) {
-            items(100) {
-                ImageListItem(it)
+                BodyContent()
             }
         }
     }
 }
 
 @Composable
-fun ImageListItem(index: Int) {
-
-    Row( ) {
-        Image(
-            painter = rememberImagePainter(
-                data = "https://developer.android.com/images/brand/Android_Robot.png"
-            ),
-            contentDescription = "Android Logo",
-            modifier = Modifier.size(50.dp)
-        )
-        Spacer(Modifier.width(10.dp))
-        Text("Item $index", style = MaterialTheme.typography.subtitle1)
+fun BodyContent(modifier: Modifier = Modifier) {
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
     }
+}
 
-
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val placeable = measurables.map { it.measure(constraints) }
+        var yPosition = 0
+        layout(constraints.maxWidth,constraints.maxHeight) {
+            placeable.forEach{ placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+                yPosition += placeable.height
+            }
+        }
+    }
 }
